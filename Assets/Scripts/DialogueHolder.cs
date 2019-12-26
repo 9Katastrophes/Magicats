416af7eaa0b3f9e8f6b33c844d5a6e01137a2e51
Katherine Wang <kat@Katherines-MBP.home>
2019-12-25 20:48:16 -0500

    Added bedroom scene. Created dialogue manager.

---
+++ Assets/Scripts/DialogueHolder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DialogueHolder : MonoBehaviour
+{
+    public DialogueManager dMan;
+    public string[] dialogueLines;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dMan = FindObjectOfType<DialogueManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Speak() {
+        if (!dMan.dialogueActive) {
+            dMan.dialogueLines = dialogueLines;
+            dMan.currentLine = 0;
+            dMan.ShowDialogue();
+        }
+    }
+}
---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dBox;
+    public Text dText;
+
+    public bool dialogueActive;
+
+    public string[] dialogueLines;
+    public int currentLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLine = 0;
+        dialogueLines = new string[] {""};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogueActive && Input.GetMouseButtonDown(0)) {
+            currentLine++;
+        }
+        if (currentLine >= dialogueLines.Length) {
+            dBox.SetActive(false);
+            dialogueActive = false;
+            currentLine = 0;
+        }
+
+        dText.text = dialogueLines[currentLine];
+    }
+
+    public void ShowDialogue() {
+        dialogueActive = true;
+        dBox.SetActive(true);
+    }
+}
